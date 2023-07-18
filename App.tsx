40b2bc51778ee7a41a3c22cbb6befb4741b85ac3
fikri hanif <fikrihanif84@gmail.com>
2023-07-18 20:38:05 +0700

    first commit

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React, {useState} from 'react';
+import {View, Text, TextInput} from 'react-native';
+
+export default function App() {
+  const [email, setEmail] = useState('');
+
+  return (
+    <View>
+      <Text>Bismillah</Text>
+    </View>
+  );
+}
